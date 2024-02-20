53441217b8c7f5a0f0425c92341cac0e57d1cb29
RunaArima <irenelovelace000@gmail.com>
2024-02-20 16:25:39 +0900

    create items.json file

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import pathlib
+import json
+import requests
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,12 +25,18 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
+items = []
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
+def add_item(name: str = Form(...),category: str = Form(...)):
     logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+    item = {"name": name, "category": category}
+    items.append = item
+    return {"message": f"item received: {item}"}
 
+@app.get("/items")
+def get_items():
+    return{"items":  item}
 
 @app.get("/image/{image_name}")
 async def get_image(image_name):
