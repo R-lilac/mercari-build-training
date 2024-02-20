b5c16b02935d7aaeecbf233ee7f15da4d91bb1db
RunaArima <irenelovelace000@gmail.com>
2024-02-20 19:58:12 +0900

    add endpoint

--- python/main.py
+++ python/main.py
@@ -3,14 +3,17 @@ import logging
 import pathlib
 import json
 import requests
-from fastapi import FastAPI, Form, HTTPException
+import hashlib
+from fastapi import FastAPI, Form, HTTPException,File, UploadFile
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 logger = logging.getLogger("uvicorn")
-logger.level = logging.INFO
+# logger level INFO->DEBUG
+logger.level = logging.DEBUG
 images = pathlib.Path(__file__).parent.resolve() / "images"
+items_file = pathlib.Path(__file__).parent.resolve() / "items.json"
 origins = [os.environ.get("FRONT_URL", "http://localhost:3000")]
 app.add_middleware(
     CORSMiddleware,
@@ -25,18 +28,61 @@ app.add_middleware(
 def root():
     return {"message": "Hello, world!"}
 
-items = []
 
 @app.post("/items")
-def add_item(name: str = Form(...),category: str = Form(...)):
+def add_item(name: str = Form(...),category: str = Form(...),image: UploadFile = File(...)):
     logger.info(f"Receive item: {name}")
-    item = {"name": name, "category": category}
-    items.append = item
-    return {"message": f"item received: {item}"}
+    logger.info(f"Receive item: {category}")
+    logger.info(f"Receive item: {image}")
+
+    image_name = image.filename
+    hashed_image = get_hash(image_name)
+
+    save_items_in_file(name,category,hashed_image) 
+    save_image(image,hashed_image) 
+
+    return {"message": f"item received: {name}"}
+
+def save_items_in_file(name,category,image_name):
+    new_item = {"name": name, "category": category, "image_name": image_name}
+    if os.path.exists(items_file):
+        with open(items_file,'r') as f:
+            now_data = json.load(f)
+        if new_item in now_data["items"]:
+            return 
+        else:
+            now_data["items"].append(new_item)
+        with open(items_file, 'w') as f:
+            json.dump(now_data, f, indent=2)
+    else:
+        first_item = {"items": [new_item]}
+        with open(items_file, 'w') as f:
+            json.dump(first_item, f, indent=2)
+
+def save_image(image,jpg_hashed_image_name):
+    imagefile = image.file.read()
+    image = images / jpg_hashed_image_name
+    with open(image, 'wb') as f:
+        f.write(imagefile)
+    return
+
+def get_hash(image):
+    hash = hashlib.sha256(image.encode()).hexdigest()
+    return hash+".jpg"
 
 @app.get("/items")
 def get_items():
-    return{"items":  item}
+    with open(items_file) as f:
+        items = json.load(f)
+    return items
+
+@app.get("/items/{item_id}")
+def get_items_item(item_id: int):
+    with open(items_file) as f:
+        items = json.load(f)
+    if item_id < 0 or item_id > len(items["items"]):
+        return {"message": "item not found"}
+    return items["items"][item_id-1]
 
 @app.get("/image/{image_name}")
 async def get_image(image_name):
@@ -51,3 +97,4 @@ async def get_image(image_name):
         image = images / "default.jpg"
 
     return FileResponse(image)
+
